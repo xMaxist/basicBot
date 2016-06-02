ac09df133dd293d90cb9eb5b995e06a39eff383d
MrMaxx <steffennoppe@hotmail.com>
2016-06-02 21:30:40 +0200

    Update basicBot.js

--- basicBot.js
+++ basicBot.js
@@ -2637,20 +2637,7 @@
                     }
                 }
             },
-            
-            bot.commands.baconCommand = {
-            command: 'pizza',  //The command to be called. With the standard command literal this would be: !bacon
-            rank: 'user', //Minimum user permission to use the command
-            type: 'exact', //Specify if it can accept variables or not (if so, these have to be handled yourself through the chat.message
-            functionality: function (chat, cmd) {
-                if (this.type === 'exact' && chat.message.length !== cmd.length) return void (0);
-                if (!bot.commands.executable(this.rank, chat)) return void (0);
-                else {
-                    API.sendChat("/me ey b0ss can i habe pizza pls ");
-                }
-            }
-        };
-
+         
             statusCommand: {
                 command: 'status',
                 rank: 'bouncer',
